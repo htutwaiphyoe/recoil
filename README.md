1c36b571ca086db8840f6bef3105b30fea1be89e
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-19 15:15:03 +0630

    #add: jsconfig

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sneaker Hub - E-commerce Web Application for sneaker shop
+
+Website: [sneakerhub]()
+
+## Table of contents
+
+- Technologies
+- Features
+
+### Technologies
+
+- NextJS
+- ReactJS
+- Sass
--- next.config.js
+++ next.config.js
@@ -11,25 +11,3 @@ module.exports = {
         includePaths: [path.join(__dirname, "sass")],
     },
 };
-
-// old config
-// module.exports = {
-//     webpack: (config) => {
-//         config.node = {
-//             fs: "empty",
-//         };
-//         return config;
-//     },
-// };
-
-// disable config
-// module.exports = {
-//     webpack: (config) => {
-//         config.node = {
-//             global: false,
-//             __filename: false,
-//             __dirname: false,
-//         };
-//         return config;
-//     },
-// };
