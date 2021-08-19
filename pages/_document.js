49127fd8d689ba427ef21e4d104bb01ba7b5e824
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-19 15:31:45 +0630

    #add: css variables

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,10 @@ class DocumentStructure extends Document {
                     href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,900&display=swap"
                     rel="stylesheet"
                 />
-
+                <link
+                    href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css"
+                    rel="stylesheet"
+                ></link>
                 <body>
                     <Main />
                     <NextScript />
