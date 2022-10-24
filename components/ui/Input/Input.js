b66ede375a59e6b94ef41da441e022c98e17fc76
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-10-24 13:55:36 +0630

    chore: repo clean up

--- components/ui/Input/Input.js
+++
@@ -1,7 +0,0 @@
-const Input = (props) => {
-    let input = null;
-    switch (props.inputType) {
-        default:
-            input = <input {...props.inputConfig} />;
-    }
-};
--- pages/login.js
+++
@@ -1,7 +0,0 @@
-import Login from "../components/auth/Login/Login";
-
-const LoginPage = (props) => {
-    return <Login />;
-};
-
-export default LoginPage;
